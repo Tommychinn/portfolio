e1938c34133e061631086b156e783c6696bcc329
Tommmychinn <tommychinn1@gmail.com>
2020-06-14 20:11:37 +0100

    adding experience

--- src/components/Cv/Cv.jsx
+++ src/components/Cv/Cv.jsx
@@ -8,11 +8,13 @@ export default function Cv() {
             <Row>
                 <Col lg={{ size: 3, offset: 1 }} className={styles.leftCol}>
                     <Row>
-                        <img
-                            className={styles.photo}
-                            src={require("./download.png")}
-                            alt="profile"
-                        />
+                        <Container>
+                            <img
+                                className={styles.photo}
+                                src={require("./download.png")}
+                                alt="profile"
+                            />
+                        </Container>
                     </Row>
                     <Row>Summary</Row>
                     <Row>Mobile</Row>
@@ -22,8 +24,40 @@ export default function Cv() {
                 <Col lg={{ size: 6, offset: 1 }} className={styles.rightCol}>
                     <Row>
                         <h3>Work Experience</h3>
-                        <Col></Col>
-                        <Col></Col>
+                        <p></p>
+                        <Col lg="2"></Col>
+                        <Col lg="10">
+                            <Row>
+                                <h4>
+                                    Operations Manager @ SkiBro Technologies
+                                    Ltd, Val d’Isère, London, Jersey
+                                </h4>
+                                <h5>DEC 2017 – JUL 2019</h5>
+                                <li>
+                                    Ran operations for first year of launch for
+                                    ski & snowboard lesson aggregator platform
+                                </li>
+                                <li>
+                                    Head relationship manager of lesson service
+                                    providers
+                                </li>
+                                <li>
+                                    Head of BD campaign to grow service provider
+                                    network
+                                </li>
+                                <li>
+                                    Product owner of backend management system
+                                    and service provider apps. Closely worked
+                                    with tech team to design & build products
+                                </li>
+                                <li>
+                                    Managed finances and designed service
+                                    provider payment system
+                                </li>
+                            </Row>
+                            <Row></Row>
+                            <Row></Row>
+                        </Col>
                     </Row>
                     <Row>Education</Row>
                     <Row>Interests</Row>
